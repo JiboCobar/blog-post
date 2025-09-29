ed50263fd3ffc01fe0ed2e75da85df5220185667
JiboCobar <cobar.jesusemmanueljr@gmail.com>
2025-09-29 16:42:51 +0800

    blog-post

---
+++ app/Filament/Pages/Profile.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Auth\Pages\EditProfile;
+use Filament\Forms\Components\FileUpload;
+use Filament\Pages\Page;
+use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Component;
+use Illuminate\Validation\Rules\Password;
+
+class Profile extends EditProfile
+{
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                FileUpload::make('avatar')
+                    ->avatar()
+                    ->imageEditor()
+                    ->disk('public')
+                    ->visibility('public')
+                    ->circleCropper(),
+
+                    /*
+                    ->required()
+                    ->rule('required')
+                    ->rules(['required']),    
+                    */ 
+
+                $this->getNameFormComponent(),
+                $this->getEmailFormComponent(),
+                $this->getPasswordFormComponent(),
+                $this->getPasswordConfirmationFormComponent(),
+                $this->getCurrentPasswordFormComponent(),
+            ]);
+    }
+
+    protected function getPasswordFormComponent(): Component
+    {
+        return parent::getPasswordFormComponent()
+            ->rules([
+                Password::min(8)
+                    ->max(12)
+                    ->symbols()
+                    ->mixedCase()
+                    ->letters()
+                    ->numbers()
+                    ->uncompromised()
+            ]);
+    }
+}
---
+++ app/Providers/Filament/AppPanelProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use App\Filament\Pages\Profile;
+use Filament\Auth\MultiFactor\App\AppAuthentication;
+use Filament\Auth\MultiFactor\Email\EmailAuthentication;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\AuthenticateSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages\Dashboard;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets\AccountWidget;
+use Filament\Widgets\FilamentInfoWidget;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+class AppPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->default()
+            ->databaseNotifications()
+            ->sidebarCollapsibleOnDesktop()
+            ->id('app')
+            ->path('app')
+            ->login()
+            ->registration()
+            ->profile(page: Profile::class, isSimple: false)
+            ->passwordReset()
+            ->multiFactorAuthentication([
+                AppAuthentication::make()->recoverable(),
+                EmailAuthentication::make(),
+            ])
+            ->emailVerification()
+            ->colors([
+                'primary' => Color::Indigo,
+            ])
+            ->discoverResources(in: app_path('Filament/App/Resources'), for: 'App\Filament\App\Resources')
+            ->discoverPages(in: app_path('Filament/App/Pages'), for: 'App\Filament\App\Pages')
+            ->pages([
+                Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/App/Widgets'), for: 'App\Filament\App\Widgets')
+            ->widgets([
+                AccountWidget::class,
+                FilamentInfoWidget::class,
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
+    }
+}
